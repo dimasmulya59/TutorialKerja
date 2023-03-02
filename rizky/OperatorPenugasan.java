2733be5158d23c4b0060f8530f591c436cf74c00
rizkydarmawan89 <rizki.darmawan.koagow@gmail.com>
2023-03-02 07:51:08 +0700

    Operator Penugasan

---
+++ rizky/OperatorPenugasan.java
@@ -0,0 +1,26 @@
+package rizky;
+
+public class OperatorPenugasan {
+    public static void main(String[] args) {
+        // Operator Penugasan jumlah, kurang, perkalian, pembagian, modulus
+        int a = 1;
+        a += 5;
+        System.out.println("nilai a = " + a);
+
+        int b = 10;
+        b -= 5;
+        System.out.println("nilai a = " + b);
+
+        int c = 1;
+        c *= 5;
+        System.out.println("nilai a = " + c);
+
+        int d = 10;
+        d /= 5;
+        System.out.println("nilai a = " + d);
+
+        int e = 10;
+        e %= 5;
+        System.out.println("nilai a = " + e);
+    }
+}
