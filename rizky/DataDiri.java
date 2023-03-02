ecfbedf6788452122c8d68414fa2f0ca14516a07
rizkydarmawan89 <rizki.darmawan.koagow@gmail.com>
2023-03-02 07:26:45 +0700

    Data Diri

---
+++ rizky/DataDiri.java
@@ -0,0 +1,22 @@
+package rizky;
+
+public class DataDiri {
+    public static void main(String[] args) {
+        String nama, alamat, hobi, kuliah;
+        int umur, tinggi;
+
+        nama = "Rizky";
+        alamat = "alamat";
+        hobi = "Dagang";
+        kuliah = "STMIK PRANATA INDONESIA";
+        umur = 33;
+        tinggi = 170;
+
+        System.out.println("NAMA ANDA   = " + nama);
+        System.out.println("ALAMAT ANDA = " + alamat);
+        System.out.println("HOBI ANDA   = " + hobi);
+        System.out.println("Kuliah = " + kuliah);
+        System.out.println("UMUR ANDA   = " + umur + "Tahun");
+        System.out.println("TINGGI ANDA = " + tinggi + "Cm");
+    }
+}
